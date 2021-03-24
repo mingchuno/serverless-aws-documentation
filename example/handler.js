288ba7761588cd7b17d5edf0e87d9a14b2d4ffaa
mingchun.or <mingchuno@gmail.com>
2021-03-24 14:45:49 +0800

    style: fix example linting

--- example/handler.js
+++ example/handler.js
@@ -4,53 +4,53 @@ const responseHeadersJson = {
   'Access-Control-Allow-Origin': '*', // Required for CORS support to work
   'Access-Control-Allow-Credentials': true,
   'Access-Control-Expose-Headers': 'link',
-  'Content-Type': "'application/json'"
+  'Content-Type': '\'application/json\'',
 }
 
 module.exports.router = (event, context, callback) => {
   const routes = {
     '/example/message': exampleMessageGet,
-    '/example/do-something': exampleDoSomethingPost
+    '/example/do-something': exampleDoSomethingPost,
   }
-  let handler = routes[event.path]
+  const handler = routes[event.path]
   const response = handler(event, context, callback)
   callback(null, response)
 }
 
 function exampleMessageGet (event, context, callback) {
-  let headers = responseHeadersJson
-  let scheme = event.headers['X-Forwarded-Proto']
-  let host = event.headers.Host
-  let path = event.requestContext.path.replace('example/message','example/do-something')
+  const headers = responseHeadersJson
+  const scheme = event.headers['X-Forwarded-Proto']
+  const host = event.headers.Host
+  const path = event.requestContext.path.replace('example/message','example/do-something')
   headers.link = `<${scheme}://${host}${path}>; rel="related"`
   return {
     statusCode: 200,
     headers: headers,
     body: JSON.stringify({
       message: 'Hello, World!',
-    })
+    }),
   }
 }
 
 function exampleDoSomethingPost (event, context, callback) {
-  let body = JSON.parse(event.body)
+  const body = JSON.parse(event.body)
   if (body.constructor !== Array) {
     return {
       statusCode: 400,
       headers: responseHeadersJson,
       body: JSON.stringify({
         message: 'The supplied request body must be a JSON array',
-        statusCode: '400'
-      })
+        statusCode: '400',
+      }),
     }
   }
-  let itemCount = body.length
+  const itemCount = body.length
   return {
     statusCode: 200,
     headers: responseHeadersJson,
     body: JSON.stringify({
       result: 'Thanks for sending that data',
-      submittedItems: itemCount
-    })
+      submittedItems: itemCount,
+    }),
   }
 }
